8314a2a35b06584ff694bc7186f8600fda7f06b8
ajainarayanan <ajainarayanan@gmail.com>
2013-11-06 20:22:39 -0800

    Minor Backbone changes

--- scripts/post.js
+++ scripts/post.js
@@ -1,15 +1,12 @@
 var Backbone = require("backbone");
 var template = require("../tmpl/template")();
 module.exports = Backbone.View.extend({
-    attributes:{
-        "class": "post"
-    },
     initialize: function() {
         this.render();
         return this;
     },
     render: function() {
-        this.$el.append(template);
+        this.$el = $(template);
         return this;
     }
 })
